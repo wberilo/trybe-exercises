30c9f15f92311a466128f9d46f157dcfc619d47a
Berilo <wberilo@gmail.com>
2020-08-18 18:00:21 -0300

    Initial commit

---
+++ my-shell-scripts/elefantes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in shell script utilizando a estrutura repetição for no terminal "aaa \n aaaa"; do
+ echo "$i"
+done
+echo $'hello\a\a\a\aworld'
---
+++ my-shell-scripts/exerc1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+VAR="/home/berilo/Desktop/16 /home/berilo/Desktop/unixstuff";
+VAR="$VAR /home/berilo/.sudo_as_admin_successful"
+for i in $VAR; do
+echo $i
+if [ -e $i ]
+then
+echo "o caminho está habilitado";
+if [ -w $i ]
+then
+echo "você também pode editar";
+fi
+if [ -d $i ]
+then
+echo "directory has" $( ls $i | wc -l ) "files";
+fi
+fi
+done
