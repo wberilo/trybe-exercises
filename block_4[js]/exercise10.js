1f46a71e9025cd53d43c14a2f5edfcdeb1ad9021
Berilo <wberilo@gmail.com>
2020-08-27 18:23:00 -0300

    exercicios completos, fim

---
+++ block_4[js]/exercise10.js
@@ -0,0 +1,11 @@
+let custo = 5;
+let valor = 10;
+
+custo < 0 || valor < 0 ? console.log("erro") : console.log();
+
+let custototal = custo + custo/5;
+
+let lucro = valor-custototal;
+
+console.log("A empresa terá ", lucro, " de lucro com uma venda")
+console.log("A empresa terá ", lucro*100 , " de lucro com 1000 vendas")
