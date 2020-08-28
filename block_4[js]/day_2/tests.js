2551c1b090e4f28fc814782004bac167686f256a
Berilo <wberilo@gmail.com>
2020-08-28 19:24:56 -0300

    codeclimate files

--- block_4[js]/day_2/tests.js
+++ block_4[js]/day_2/tests.js
@@ -1,8 +1,12 @@
 let numbers =[5,9,3,19,70,8,100,2,35,27]
 
-console.log(numbers)
+let saved = numbers.splice(7,4,2,3,3,"f")
 
+console.log(numbers)
+console.log(saved)
+/*
 for(let b in numbers){
     console.log(numbers[b])
 }
 console.log(numbers.indexOf(100))
+*/
