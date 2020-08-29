09cb00694383ec563620249c6f7dd68909bc2617
wberilo <wberilo@gmail.com>
2020-08-28 22:17:51 -0300

    Update exercise7.js

--- block_4/day_2/exercise7.js
+++ block_4/day_2/exercise7.js
@@ -2,6 +2,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 50;
 
 for (let index = 0; index < numbers.length; index++) {
-  result >= numbers[index] ? (result = numbers[index]) : (result = result);
+  if(result >= numbers[index]){
+    result = numbers[index]
+  }
 }
 console.log(result);
