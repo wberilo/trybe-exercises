b8295f88b6edd938655d9678105771587b95c97a
Berilo <wberilo@gmail.com>
2020-08-28 21:53:12 -0300

    more style fixes

--- block_4/day_2/Bonus1.js
+++ block_4/day_2/Bonus1.js
@@ -1,14 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let a = 1; a < numbers.length; a+=1) {
-  for (let j = 0; j < numbers.length - a; j+=1 ) {
+for (let a = 1; a < numbers.length; a += 1) {
+  for (let j = 0; j < numbers.length - a; j += 1) {
     if (numbers[j] > numbers[j + 1]) {
       let c = numbers[j];
       numbers[j] = numbers[j + 1];
-      numbers[j + 1] = a;
+      numbers[j + 1] = c;
     }
   }
 }
-for (let index = 0; index < numbers.length; index+=1) {
+
+for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
--- block_4/day_2/Bonus2.js
+++ block_4/day_2/Bonus2.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let i = 1; i < numbers.length; i+=1 ) {
-  for (let j = 0; j < numbers.length - i; j+=1 ) {
+for (let i = 1; i < numbers.length; i += 1) {
+  for (let j = 0; j < numbers.length - i; j += 1) {
     if (numbers[j] < numbers[j + 1]) {
       let a = numbers[j];
       numbers[j] = numbers[j + 1];
@@ -9,6 +9,7 @@ for (let i = 1; i < numbers.length; i+=1 ) {
     }
   }
 }
-for (let index = 0; index < numbers.length; index+=1 ) {
+
+for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
