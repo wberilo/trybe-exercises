e82115d6c0adce445e8c4b2c44d5f89a25a28b60
Berilo Wanderley <wberilo@gmail.com>
2020-09-01 16:41:29 -0300

    formatting changes

--- block_4/day_4/p2-exercises.js
+++ block_4/day_4/p2-exercises.js
@@ -11,7 +11,7 @@ console.log(verificaPalindrome('darara'));
 function maiorValor(arr) {
   let out = -1;
   let lov = 0;
-  for (i in arr) {
+  for (let i in arr) {
     if (lov < arr[i]) {
       lov = arr[i];
       out = i;
@@ -24,7 +24,7 @@ console.log(maiorValor([2, 3, 6, 7, 10, 1]));
 function menorValor(arr) {
   let out = -1;
   let lov = 100;
-  for (i in arr) {
+  for (let i in arr) {
     if (lov > arr[i]) {
       lov = arr[i];
       out = i;
@@ -37,10 +37,10 @@ console.log(menorValor([2, 4, 6, 7, 10, 0, -3]));
 function mostLetters(names) {
   let cou = 0;
   let out = '';
-  for (name in names) {
-    if (names[name].length > cou) {
-      cou = names[name].length;
-      out = names[name];
+  for (let nameb in names) {
+    if (names[nameb].length > cou) {
+      cou = names[nameb].length;
+      out = names[nameb];
     }
   }
   return out;
@@ -62,8 +62,7 @@ function stringer(word, ending) {
   let end = word.slice(-ending.length);
   if (end == ending) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 console.log(stringer('trybe', 'be'));
