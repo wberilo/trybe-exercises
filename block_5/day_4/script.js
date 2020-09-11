ec4c083b8892f366b96b0ae42640f0b5d7247647
Berilo Wanderley <wberilo@gmail.com>
2020-09-10 23:17:31 -0300

    rollback

--- block_5/day_4/script.js
+++ block_5/day_4/script.js
@@ -13,49 +13,46 @@ for (let i = 0; i < change.length; i++) {
   change[i].style.lineHeight = spacing;
   change[i].style.fontFamily = ffamily;
 }
-function run() {
-  for (let i = 0; i < change.length; i++) {
-    change[i].style.fontFamily = ffamily;
-    change[i].style.lineHeight = spacing;
-    change[i].style.fontSize = fonts;
-    change[i].style.color = textcolor;
-  }
-}
-function store(str,value){
-    localStorage.setItem(str, value);
-}
 document
   .querySelector("#bgcolor-button")
   .addEventListener("click", function () {
     bgcolor = document.getElementById("bgcolor-input").value;
     document.body.style.backgroundColor = bgcolor;
-    store('bgcolor',bgcolor)
+    localStorage.setItem("bgcolor", bgcolor);
   });
 document
   .querySelector("#textcolor-button")
   .addEventListener("click", function () {
     textcolor = document.getElementById("textcolor-input").value;
-    run();
-    store("textcolor", textcolor);
+    for (let i = 0; i < change.length; i++) {
+      change[i].style.color = textcolor;
+    }
+    localStorage.setItem("textcolor", textcolor);
   });
 document
   .querySelector("#fontsize-button")
   .addEventListener("click", function () {
     fonts = document.getElementById("fonts-input").value + "px";
-    run();
-    store("fontsize", fonts);
+    for (let i = 0; i < change.length; i++) {
+      change[i].style.fontSize = fonts;
+    }
+    localStorage.setItem("fontsize", fonts);
   });
 document
   .querySelector("#spacing-button")
   .addEventListener("click", function () {
     spacing = document.getElementById("spacing-input").value + "px";
-    run();
-    store("spacing", spacing);
+    for (let i = 0; i < change.length; i++) {
+      change[i].style.lineHeight = spacing;
+    }
+    localStorage.setItem("spacing", spacing);
   });
 document
   .querySelector("#ffamily-button")
   .addEventListener("click", function () {
     ffamily = document.getElementById("ffamily-input").value;
-    run();
-    store("ffamily", ffamily);
+    for (let i = 0; i < change.length; i++) {
+      change[i].style.fontFamily = ffamily;
+    }
+    localStorage.setItem("ffamily", ffamily);
   });
