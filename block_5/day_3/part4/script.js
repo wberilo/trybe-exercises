68d8f04825ee458325657f9c5ef5b909def5624e
Berilo Wanderley <wberilo@gmail.com>
2020-09-11 21:56:49 -0300

    cc

--- block_5/day_3/part4/script.js
+++ block_5/day_3/part4/script.js
@@ -1,33 +1,32 @@
-var personName = "";
+var personName = '';
 
 function complainMessage() {
-  alert("I told you not to do that " + personName);
+  alert('I told you not to do that ' + personName);
 }
-let a = document.querySelector('button')
-a.addEventListener('click',complainMessage)
+let a = document.querySelector('button');
+a.addEventListener('click', complainMessage);
 
 function typeAndScream() {
-  document.getElementById("audio").play();
-  personName = document.getElementById("name").value;
+  document.getElementById('audio').play();
+  personName = document.getElementById('name').value;
 }
-let b = document.querySelector('.textEntry')
-b.addEventListener('input',typeAndScream)
+let b = document.querySelector('.textEntry');
+b.addEventListener('input', typeAndScream);
 
 function goDark() {
-  var body = document.getElementsByTagName("body")[0];
-  if (body.className == "") {
-    body.className = "dark";
+  var body = document.getElementsByTagName('body')[0];
+  if (body.className == '') {
+    body.className = 'dark';
+  } else {
+    body.className = '';
   }
-  else {
-    body.className = "";
-  }
-  
 }
-let c = document.getElementsByTagName('button')[1]
-c.addEventListener('click',goDark)
+let c = document.getElementsByTagName('button')[1];
+c.addEventListener('click', goDark);
 
 function finishTheStory() {
-  document.getElementById("story").innerHTML = "So I carefully searched the damp dungeon. There you are! I grinned a smile with all 98 of my gleaming teeth. Dinner time!";
+  document.getElementById('story').innerHTML =
+    'So I carefully searched the damp dungeon. There you are! I grinned a smile with all 98 of my gleaming teeth. Dinner time!';
 }
-let d = document.getElementsByTagName('button')[2]
-d.addEventListener('click',finishTheStory)
+let d = document.getElementsByTagName('button')[2];
+d.addEventListener('click', finishTheStory);
