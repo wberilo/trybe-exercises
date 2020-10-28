3a462e3346b22eca226000249653b83438f404d8
Berilo Wanderley <wberilo@gmail.com>
2020-10-28 15:18:10 -0300

    dependencies + name fixes to fix jest

---
+++ block_10/tests/sum.test.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => a + b;
+
+describe('ay',()=>{
+  test('sums two numbers', () => {
+    expect(sum(2, 3)).toEqual(5);
+  });
+})
+
+describe('add',()=>{
+  test('sums two YEAAAAAAAAAAAAAAAAAAAAH', () => {
+    expect(sum(2, 3)).toEqual(5);
+  });
+})
