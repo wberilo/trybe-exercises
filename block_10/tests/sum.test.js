3f1af318896e52233848cd298da2334dfd5db2a3
Berilo Wanderley <wberilo@gmail.com>
2020-10-29 14:38:16 -0300

    ex

--- block_10/tests/sum.test.js
+++ block_10/tests/sum.test.js
@@ -1,13 +1,22 @@
 const sum = (a, b) => a + b;
 
-describe('ay',()=>{
+describe('case1',()=>{
   test('sums two numbers', () => {
     expect(sum(2, 3)).toEqual(5);
   });
 })
 
 describe('add',()=>{
-  test('sums two YEAAAAAAAAAAAAAAAAAAAAH', () => {
+  test('sums two numbers', () => {
     expect(sum(2, 3)).toEqual(5);
+  });
+  test('sums two numbers', () => {
+    expect(sum(2, 3)).toEqual(5);
+  });
+  test('sums two numbers', () => {
+    expect(sum(2, 3)).toEqual(5);
+  });
+  test('sums two numbers', () => {
+    expect(sum(2, 455)).toEqual(5);
   });
 })
